2009f9891b9f990b219f8641ee74968b6ba3caae
abikemechanic <aaronriechman@gmail.com>
2022-08-15 21:21:25 -0400

    adding to subclasses

--- image_creator.py
+++ image_creator.py
@@ -2,6 +2,19 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 class ImageCreator:
+    small_font = ImageFont.truetype(r'fonts/Courier Prime Bold.ttf', 12)
+    large_font = ImageFont.truetype(r'fonts/Courier Prime Bold.ttf', 20)
+
     def __init__(self, pixel_width=250, pixel_height=122):
         self.pixel_width = pixel_width
         self.pixel_height = pixel_height
+
+        self.image = Image.new('1', (self.pixel_height, self.pixel_width), 255)
+        self.draw = ImageDraw.Draw(self.image)
+
+    def add_text(self, text: str):
+        self.draw.text((0, 0), text, font=self.large_font, fill=0)
+
+    def clear_image(self):
+        self.image = Image.new('1', (self.pixel_height, self.pixel_width), 255)
+        self.draw = ImageDraw.Draw(self.image)
